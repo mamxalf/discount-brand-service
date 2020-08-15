35c85ea8730a6c69e58920e90965efe04eaeba0a
mamxalf <hammamxalf@gmail.com>
2020-08-15 20:58:39 +0700

    create database

--- database/migrations/2020_08_15_133648_create_brand_media_table.php
+++ database/migrations/2020_08_15_133648_create_brand_media_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBrandsTable extends Migration
+class CreateBrandMediaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateBrandsTable extends Migration
      */
     public function up()
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('brand_media', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
+            $table->bigInteger('id_brand')->unsigned();
+            $table->string('logo');
             $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign("id_brand")->references("id")->on("brands")->onDelete('cascade');
         });
     }
 
@@ -28,6 +31,6 @@ class CreateBrandsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('brand_media');
     }
 }
