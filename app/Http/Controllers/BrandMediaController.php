c8e7d4024af3793c08df56880de7c08815cb989f
mamxalf <hammamxalf@gmail.com>
2020-08-15 23:50:18 +0700

    controller brandmedia

--- app/Http/Controllers/BrandMediaController.php
+++ app/Http/Controllers/BrandMediaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\BrandMedia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BrandMediaController extends Controller
 {
@@ -15,6 +17,13 @@ class BrandMediaController extends Controller
     public function index()
     {
         //
+        $data = BrandMedia::with('brand')->get();
+
+        if ( $data ) {
+            return ResponseFormatter::sucess($data, 'Data Brands berhasil diambil!');
+        } else {
+            return ResponseFormatter::error(null, 'Data Brand tidak ada!', 404);
+        }
     }
 
     /**
@@ -35,7 +44,28 @@ class BrandMediaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'id_brand' => 'required|integer',
+            'logo' => 'required|file'
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error($validator->errors(), 'Ada kolom terlewat!', 401);
+        } else {
+            $data = new BrandMedia();
+            $data->id_brand = $request->get('id_brand');
+            if($request->file('logo')){
+                $file = $request->file('logo')->store('logo', 'public');
+                $data->logo = $file;
+            }
+            $data->save();
+
+            if ($data) {
+                return ResponseFormatter::sucess($data, "Data berhasil ditambahkan!");
+            } else {
+                return ResponseFormatter::error($validator->errors(), 'Ada kolom terlewat!', 400);
+            }
+        }
     }
 
     /**
@@ -44,9 +74,16 @@ class BrandMediaController extends Controller
      * @param  \App\BrandMedia  $brandMedia
      * @return \Illuminate\Http\Response
      */
-    public function show(BrandMedia $brandMedia)
+    public function show(BrandMedia $brandMedia, $brand_medium)
     {
         //
+        $data = BrandMedia::with('brand')->find($brand_medium);
+
+        if ( $data ) {
+            return ResponseFormatter::sucess($data, 'Data Brands berhasil diambil!');
+        } else {
+            return ResponseFormatter::error(null, 'Data Brand tidak ada!', 404);
+        }
     }
 
     /**
@@ -57,7 +94,7 @@ class BrandMediaController extends Controller
      */
     public function edit(BrandMedia $brandMedia)
     {
-        //
+
     }
 
     /**
@@ -67,9 +104,35 @@ class BrandMediaController extends Controller
      * @param  \App\BrandMedia  $brandMedia
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, BrandMedia $brandMedia)
+    public function update(Request $request, BrandMedia $brandMedia, $brand_medium)
     {
         //
+//        $validator = Validator::make($request->all(),[
+//            'id_brand' => 'required|integer',
+//            'logo' => 'required|file'
+//        ]);
+//
+//        if ($validator->fails()) {
+//            return ResponseFormatter::error($validator->errors(), 'Ada kolom terlewat!', 401);
+//        } else {
+//            $data = BrandMedia::find($brand_medium)->first();
+//            $data->id_brand = $request->input('id_brand');
+//            if($request->file('logo')){
+//                if($data->logo && file_exists(storage_path('app/public/' . $data->logo))){
+//                    \Storage::delete('public/'.$data->logo);
+//                }
+//
+//                $file = $request->file('logo')->store('logo', 'public');
+//                $data->logo = $file;
+//            }
+//            $data->save();
+//
+//            if ($data) {
+//                return ResponseFormatter::sucess($data, "Data berhasil ditambahkan!");
+//            } else {
+//                return ResponseFormatter::error($validator->errors(), 'Ada kolom terlewat!', 400);
+//            }
+//        }
     }
 
     /**
@@ -78,8 +141,20 @@ class BrandMediaController extends Controller
      * @param  \App\BrandMedia  $brandMedia
      * @return \Illuminate\Http\Response
      */
-    public function destroy(BrandMedia $brandMedia)
+    public function destroy(BrandMedia $brandMedia, $brand_medium)
     {
-        //
+        $file = BrandMedia::with('brand')->find($brand_medium);
+        Storage::delete('public/'.$file->logo);
+
+//        return ResponseFormatter::sucess($file, "Data berhasil dihapus!");
+
+        $data = BrandMedia::findOrFail($brand_medium);
+        $data->delete();
+
+        if ($data) {
+            return ResponseFormatter::sucess($data, "Data berhasil dihapus!");
+        } else {
+            return ResponseFormatter::error($validator->errors(), 'Error Gan!', 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('brand', 'BrandController');
+Route::resource('brand-media', 'BrandMediaController');
